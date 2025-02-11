cf43af3a053e68bb202636c2e4b4a880ee6a3e8e
Phat Ca <phatca@hawaii.edu>
2025-02-11 09:03:42 -1000

    tried to use fake theta
    currently not working

--- TCrnTutorial/App.tsx
+++ TCrnTutorial/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import {Button, SafeAreaView, ScrollView, Text} from 'react-native';
+import {Button, SafeAreaView, ScrollView, Text, Image} from 'react-native';
 import {FileTypeEnum, initialize} from 'theta-client-react-native';
-import {getThetaInfo} from 'theta-client-react-native';
-import {listFiles} from 'theta-client-react-native';
+import {getThetaInfo, listFiles} from 'theta-client-react-native';
+
+interface FileEntry {
+  fileUrl: string;
+  name: string;
+}
 
 function App(): React.JSX.Element {
-  const [thetaInfo, setThetaInfo] = React.useState('');
+//   const [thetaInfo, setThetaInfo] = React.useState('');
+//   const [listFile, setListFile] = React.useState<FileEntry[]>([]);
 
   React.useEffect(() => {
     const initTheta = async () => {
       try {
-        await initialize('http://192.168.1.1', {});
-
+        await initialize('https://fake-theta.vercel.app/', {});
+        console.log('inititalize')
       } catch (error) {
         console.error('Failed to initialize THETA:', error);
       }
@@ -20,36 +25,50 @@ function App(): React.JSX.Element {
   }, []);
 
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <Button
-          title="info"
-          onPress={async () => {
-            try {
-              const info = await getThetaInfo();
-              setThetaInfo(JSON.stringify(info, null, 2));
-            } catch (error) {
-              console.error('Failed to get THETA info:', error);
-              setThetaInfo('Error getting THETA info');
-            }
-          }}
-        />
-        <Button
-          title="list files"
-          onPress={async () => {
-            try {
-              const files = await listFiles(FileTypeEnum.IMAGE, 1, 10);
-              setThetaInfo(JSON.stringify(files, null, 2));
-            } catch (error) {
-              console.error('Failed to get file listing:', error);
-              setThetaInfo('Error getting file listing');
-            }
-          }}
-        />
-        <Text>{thetaInfo}</Text>
-        <Text>{thetaInfo}</Text>
-      </ScrollView>
-    </SafeAreaView>
+    // <SafeAreaView>
+    //   <ScrollView>
+    //     <Button
+    //       title="info"
+    //       onPress={async () => {
+    //         try {
+    //           const info = await getThetaInfo();
+    //           setThetaInfo(JSON.stringify(info, null, 2));
+    //           setListFile([]);
+    //         } catch (error) {
+    //           console.error('Failed to get THETA info:', error);
+    //           setThetaInfo('Error getting THETA info');
+    //         }
+    //       }}
+    //     />
+    //     {/* <Button
+    //       title="list files"
+    //       onPress={async () => {
+    //         try {
+    //           const files = await listFiles(FileTypeEnum.IMAGE, 1, 10);
+    //           setListFile(files.fileList);
+    //           setThetaInfo('');
+    //         } catch (error) {
+    //           console.error('Failed to get file listing:', error);
+    //           setListFile([]);
+    //         }
+    //       }}
+    //     />
+        
+    //     {thetaInfo ? <Text>{thetaInfo}</Text> : null}
+        
+    //     {listFile.map((file) => (
+    //       <Image
+    //         key={file.name}
+    //         source={{ uri: file.fileUrl }}
+    //       />
+    //     ))} */}
+
+    //     <Button title='take photo'>
+
+    //     </Button>
+    //   </ScrollView>
+    // </SafeAreaView>
+    <Text>hello</Text>
   );
 }
 
